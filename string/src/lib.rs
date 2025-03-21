726e810fffbb3f12be728cc881c36bebac10a3a0
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-03-21 15:27:16 +0100

    feat(string): add split and strip_all methods

--- string/src/lib.rs
+++ string/src/lib.rs
@@ -2,6 +2,14 @@ pub fn strip_ctl(input: String) -> String {
     input.chars().filter(|&c| !c.is_control()).collect()
 }
 
+pub fn strip_all(input: String, search: String) -> String {
+    input.replace(&search, "")
+}
+
+pub fn split(input: String, separator: String) -> inklang_array::Of<String> {
+    input.split(&separator).map(|s| s.to_string()).collect()
+}
+
 pub fn concat(a: String, b: String) -> String {
     let mut result = String::with_capacity(a.len() + b.len());
     result.push_str(&a);
@@ -18,5 +26,27 @@ mod tests {
         let string: String = "\0\0".into();
         let result = strip_ctl(string);
         assert_eq!(result, "");
+    }
+
+    #[test]
+    fn it_strips_all() {
+        let string: String = "hello world".into();
+        let result = strip_all(string, "l".into());
+        assert_eq!(result, "heo word");
+    }
+
+    #[test]
+    fn it_splits() {
+        let string: String = "hello world".into();
+        let result = split(string, " ".into());
+        assert_eq!(result, vec!["hello".to_string(), "world".to_string()]);
+    }
+
+    #[test]
+    fn it_concats() {
+        let a: String = "hello".into();
+        let b: String = "world".into();
+        let result = concat(a, b);
+        assert_eq!(result, "helloworld");
     }
 }
