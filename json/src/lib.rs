7a1f9c1710751a2a748035ac37c75eef7c29f8b4
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-03-21 13:18:57 +0100

    fix(json): `value` to `Value`

--- json/src/lib.rs
+++ json/src/lib.rs
@@ -1,33 +1,33 @@
-pub type value = serde_json::Value;
+pub type Value = serde_json::Value;
 
-pub fn parse(json: String) -> self::value {
+pub fn parse(json: String) -> self::Value {
     serde_json::from_str(&json).unwrap()
 }
 
-pub fn get(value: self::value, key: String) -> self::value {
+pub fn get(value: self::Value, key: String) -> self::Value {
     value[key].clone()
 }
 
-pub fn as_array(value: self::value) -> Vec<self::value> {
+pub fn as_array(value: self::Value) -> Vec<self::Value> {
     value.as_array().unwrap().to_vec()
 }
 
-pub fn as_string(value: self::value) -> String {
+pub fn as_string(value: self::Value) -> String {
     value.as_str().unwrap().to_string()
 }
 
-pub fn as_boolean(value: self::value) -> bool {
+pub fn as_boolean(value: self::Value) -> bool {
     value.as_bool().unwrap()
 }
 
-pub fn as_u64(value: self::value) -> u64 {
+pub fn as_u64(value: self::Value) -> u64 {
     value.as_u64().unwrap()
 }
 
-pub fn as_f64(value: self::value) -> f64 {
+pub fn as_f64(value: self::Value) -> f64 {
     value.as_f64().unwrap()
 }
 
-pub fn as_i64(value: self::value) -> i64 {
+pub fn as_i64(value: self::Value) -> i64 {
     value.as_i64().unwrap()
 }
